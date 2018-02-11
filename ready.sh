f6e0a6c4749a3ded254869db9b67ab54ac078957
Eternal-A <15828377403@163.com>
2018-02-11 21:48:00 +0800

    取消自动脚本

--- ready.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-pip3 install -r backend/requirements
-cd frontend
-npm config set registry "http://registry.npm.taobao.org/"
-npm install
-exit 0
--- run.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-cd frontend
-npm run dev &
-cd ../backend
-python3 manage.py makemigrations
-python3 manage.py migrate
-python3 manage.py runserver
-exit 0
